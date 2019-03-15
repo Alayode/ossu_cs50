8d9e1346014d7d8720bb774fac9ad18f30acf6ad
Chris Samuel <ksamuel.chris@gmail.com>
2019-03-15 15:31:02 -0400

    print hash marks vertically

---
+++ cs50/python_examples/print_four_question_marks2.py
@@ -0,0 +1,12 @@
+# Chris K Samuel
+# @chrisksamuel
+
+
+
+# Print four question marks  => ????
+# We can print a vertical bar of hash marks, as well
+# We can print a vertical bar of hash marks, too
+
+for i in range(4):
+    print("#")
+print()
